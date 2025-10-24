984fb1e01ab733c871833eb1c9a2102f71d91942
coderpushkar05072002 <107266332+coderpushkar05072002@users.noreply.github.com>
2025-10-24 08:01:36 +0530

    Add files via upload

---
+++ hardhat.config.cjs
@@ -0,0 +1,20 @@
+﻿require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    compilers: [
+      { version: "0.8.20" },
+      { version: "0.8.17" }
+    ]
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    hardhat: { chainId: 1337 },
+    localhost: { url: "http://127.0.0.1:8545" }
+  }
+};
